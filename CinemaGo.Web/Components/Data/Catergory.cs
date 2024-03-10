159fb5cd88092410d03a20813ed409cedd8c4177
Mirlan Nurbekov <mirlan96nurbekov@gmail.com>
2024-03-10 15:41:13 +0600

    Commit 2
    Miner updates for some components

---
+++ CinemaGo.Web/Components/Data/Catergory.cs
@@ -0,0 +1,10 @@
+namespace CinemaGo.Web.Data
+{
+    public class Catergory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ CinemaGo.Web/Components/Data/UProduct.cs
@@ -0,0 +1,28 @@
+namespace CinemaGo.Web.Data
+{
+    public class UProduct
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime? DateCreated { get; set; } = DateTime.Now;
+        public DateTime? DateUpdated { get; set; }
+        public int Views { get; set; }
+
+        public int OriginalPrice { get; set; }
+
+        public int Price { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Language { get; set; }
+
+        public string RunningTime { get; set; }
+
+        public string ReleaseDate { get; set; }
+    }
+}
